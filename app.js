ca18b68a8e62cbc5529755f7b314dc0e9e38510b
EL5225 <luthfiramadhan155@gmail.com>
2023-11-25 23:28:47 +0700

    deploy vercel

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const {
   prismaErrorHandler,
   zodErrorHandler,
 } = require("./middlewares/error");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("../docs/swagger.json");
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const app = express();
 
@@ -21,6 +24,8 @@ app.set("trust proxy", true);
 
 // Routes
 app.use("/api", router);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs), { customCssUrl: CSS_URL });
 
 // Middlewares errors
 app.use(zodErrorHandler);
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const { Router } = require("express");
 const auth = require("./auth");
 const user = require("./user");
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocs = require("../docs/swagger.json");
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
 const router = Router();
 
 router.get("/", (req, res) => {
@@ -17,7 +13,5 @@ router.get("/", (req, res) => {
 
 router.use("/auth", auth);
 router.use("/user", user);
-
-router.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs), { customCssUrl: CSS_URL });
 
 module.exports = router;

a100fbf52fc2af239313a843edbb4919dfb58ec3
“nameisjar” <nthia4@gmail.com>
2023-11-27 21:23:38 +0700

    add feature/getAllUsers-getUserById

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,65 @@
 const prisma = require("../libs/prisma");
+const getPagination = require("../utils/pagination");
+
+const getUserById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+        error: null,
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "User found",
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllUsers = async (req, res, next) => {
+  try {
+    let { limit = 10, page = 1 } = req.query;
+    limit = Number(limit);
+    page = Number(page);
+
+    const users = await prisma.user.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    const { _count } = await prisma.user.aggregate({
+      _count: { id: true },
+    });
+
+    let pagination = getPagination(req, _count.id, page, limit);
+
+    if (!users) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+        error: null,
+        data: null,
+      });
+    }
+
+    res.status(200).json({
+      status: true,
+      message: "User found",
+      data: { users, pagination },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteUser = async (req, res, next) => {
   try {
@@ -26,4 +87,6 @@ const deleteUser = async (req, res, next) => {
 
 module.exports = {
   deleteUser,
+  getAllUsers,
+  getUserById,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
-const { deleteUser } = require("../controllers/user.controller");
+const { deleteUser, getAllUsers, getUserById } = require("../controllers/user.controller");
 
 const user = Router();
 
 user.delete("/:id", deleteUser);
+user.get("/", getAllUsers);
+user.get("/:id", getUserById);
 
 module.exports = user;

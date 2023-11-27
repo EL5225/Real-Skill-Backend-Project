66b727b749db0b1d37fb9a11146e034d9a527efc
faqihahfajrianij <faqihahfajriani.j@gmail.com>
2023-11-27 23:45:11 +0200

    feature update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -24,6 +24,48 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { name, phoneNumber } = req.body;
+
+    if (!name || !phoneNumber) {
+      return res.status(400).json({
+        status: false,
+        message: "Nama and No. HP harus diisi",
+      });
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: { name },
+    });
+
+    const updatedProfile = await prisma.profiles.upsert({
+      where: { user_id: id },
+      update: { phone_number: phoneNumber },
+      create: { user: { connect: { id } }, phone_number: phoneNumber },
+    });
+
+    if (!updatedUser || !updatedProfile) {
+      return res.status(404).json({
+        status: false,
+        message: "User not found",
+        error: null,
+        data: null,
+      });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: "User berhasil diperbarui",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   deleteUser,
+  updateUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { deleteUser } = require("../controllers/user.controller");
+const { deleteUser, updateUser } = require("../controllers/user.controller");
 
 const user = Router();
 
 user.delete("/:id", deleteUser);
+user.put("/users/id:", updateUser);
 
 module.exports = user;

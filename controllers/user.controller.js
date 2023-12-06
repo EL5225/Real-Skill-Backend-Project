0f865bf8d8e85bf8c4677300cc939a87a774f88c
“nameisjar” <nthia4@gmail.com>
2023-12-07 01:37:08 +0700

    change feature/create-createNotification-to-all-user-isverified(true)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -101,29 +101,59 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
+//Terkirim hanya ke 1 user
+// const createNotifications = async (req, res, next) => {
+//   try {
+//     const { title, body, user_id } = req.body;
+//     VScreateNotification.parse(req.body);
+
+//     const user = await prisma.user.findUnique({ where: { id: user_id } });
+//     if (!user) {
+//       return res.status(400).json({
+//         status: false,
+//         message: "Bad Request",
+//         data: "User Not Found",
+//       });
+//     }
+
+//     const notifications = await prisma.notifications.create({
+//       data: {
+//         title,
+//         body,
+//         user: {
+//           connect: {
+//             id: user_id,
+//           },
+//         },
+//       },
+//     });
+
+//     res.status(201).json({
+//       status: true,
+//       message: "Created Notification Success",
+//       data: notifications,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// Terkirim ke semua user
 const createNotifications = async (req, res, next) => {
   try {
-    const { title, body, user_id } = req.body;
+    const { title, body } = req.body;
     VScreateNotification.parse(req.body);
-    const user = await prisma.user.findUnique({ where: { id: user_id } });
-    if (!user) {
-      return res.status(400).json({
-        status: false,
-        message: "Bad Request",
-        data: "User Not Found",
-      });
-    }
-    const notifications = await prisma.notifications.create({
-      data: {
+
+    const users = await prisma.user.findMany({ where: { is_verified: true } });
+
+    const notifications = await prisma.notifications.createMany({
+      data: users.map((user) => ({
         title,
         body,
-        user: {
-          connect: {
-            id: user_id,
-          },
-        },
-      },
+        user_id: user.id,
+      })),
     });
+
     res.status(201).json({
       status: true,
       message: "Created Notification Success",
--- libs/validation/user.js
+++ libs/validation/user.js
@@ -1,10 +1,21 @@
 const { z } = require("zod");
 
+// const VScreateNotification = z.object({
+//   user_id: z.string({
+//     required_error: "user_id harus diisi",
+//     invalid_type_error: "user harus berupa string",
+//   }),
+//   title: z.string({
+//     required_error: "title harus diisi",
+//     invalid_type_error: "title harus berupa string",
+//   }),
+//   body: z.string({
+//     required_error: "body harus diisi",
+//     invalid_type_error: "body harus berupa string",
+//   }),
+// });
+
 const VScreateNotification = z.object({
-  user_id: z.string({
-    required_error: "user_id harus diisi",
-    invalid_type_error: "user harus berupa string",
-  }),
   title: z.string({
     required_error: "title harus diisi",
     invalid_type_error: "title harus berupa string",

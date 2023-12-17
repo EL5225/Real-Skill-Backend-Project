b589b5c21152ac362d1b47d67043bc7978c2f64f
EL5225 <luthfiramadhan155@gmail.com>
2023-12-17 21:40:21 +0700

    fix: nodemailer

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,32 +1,26 @@
 require("dotenv").config();
-const { google } = require("googleapis");
 const nodemailer = require("nodemailer");
-
-const { GOOGLE_REFRESH_TOKEN, GOOGLE_SENDER_EMAIL, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } =
-  process.env;
-
-const oauth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET);
-
-oauth2Client.setCredentials({
-  refresh_token: GOOGLE_REFRESH_TOKEN,
-});
+const { SMTP_USER, SMTP_PASS } = process.env;
 
 const sendEmail = async (to, subject, html) => {
   try {
-    const accessToken = await oauth2Client.getAccessToken();
-
     const transport = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      secure: true,
       auth: {
-        type: "OAuth2",
-        user: GOOGLE_SENDER_EMAIL,
-        clientId: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        refreshToken: GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: SMTP_USER,
+        pass: SMTP_PASS,
       },
     });
-    transport.sendMail({ to, subject, html });
+
+    const mailOptions = {
+      from: SMTP_USER,
+      to,
+      subject,
+      html,
+    };
+
+    await transport.sendMail(mailOptions);
   } catch (error) {
     console.log(error);
   }
